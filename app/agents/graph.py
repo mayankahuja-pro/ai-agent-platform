0f99bcf40c63c98dfc92388f9aeee16f8fceeb65
mayankahuja <mayankahuja785@gmail.com>
2026-08-25 14:30:37 +0530

    Add tool node and conditional edge handling to agent graph

--- app/agents/graph.py
+++ app/agents/graph.py
@@ -4,6 +4,8 @@ from langgraph.graph import (
     END,
 )
 
+from langgraph.prebuilt import ToolNode
+
 from app.agents.llm import llm
 from app.agents.state import AgentState
 from app.tools.order_tool import get_order
@@ -12,6 +14,7 @@ from app.tools.order_tool import get_order
 tools = [
     get_order,
 ]
+
 
 llm_with_tools = llm.bind_tools(tools)
 
@@ -27,21 +30,53 @@ def agent_node(state: AgentState):
     }
 
 
+def should_continue(state: AgentState):
+
+    last_message = state["messages"][-1]
+
+    if last_message.tool_calls:
+        return "tools"
+
+    return END
+
+
+tool_node = ToolNode(tools)
+
+
 builder = StateGraph(AgentState)
+
 
 builder.add_node(
     "agent",
     agent_node,
 )
 
+builder.add_node(
+    "tools",
+    tool_node,
+)
+
+
 builder.add_edge(
     START,
     "agent",
 )
 
-builder.add_edge(
+
+builder.add_conditional_edges(
     "agent",
-    END,
+    should_continue,
+    {
+        "tools": "tools",
+        END: END,
+    },
 )
+
+
+builder.add_edge(
+    "tools",
+    "agent",
+)
+
 
 agent_graph = builder.compile()
